3cd148a32c445a633e8e2a2fcf2d24cb7b266ec1
alyayes <fajrialyaya78@gmail.com>
2024-10-24 09:53:07 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
 
+public class Nomor1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-public class Nomor1{
-    public static void main(String[] args){
-      
+        System.out.println("Masukkan total penjualan salesman: Rp ");
+        double totalPenjualan = scanner.nextDouble();
+
+        double komisi;
+
+            if (totalPenjualan < 500000) {
+                komisi = totalPenjualan * 0.10;
+
+            } else {
+                komisi = 50000 * 0.10; // untuk pertama
+                komisi += (totalPenjualan - 500000) * 0.15; //untuk kedua
+            } 
+
+            System.out.printf("Total penjualan salesman: Rp %.1f%n", totalPenjualan);
+            System.out.printf("Komisi yang didapatkan: Rp %.1f%n", komisi);
+            System.out.printf("Total penjualan salesman: Rp %.1f%n", totalPenjualan);
+            System.out.printf("Komisi yang didapatkan: Rp %.1f%n", komisi);
+            }
     }
-}
